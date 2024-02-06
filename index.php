85184c5b169e12ea81a741a0f77aa3400cabe4a7
ymelki <67371192+ymelki@users.noreply.github.com>
2024-02-06 16:02:19 +0100

    ajoutdumenu

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 // session_start();
 // routeur
+include __DIR__."/vue/header.php";
 // var_dump($_SERVER);
 $page=$_GET['uc'];
 
---
+++ vue/header.php
@@ -0,0 +1,4 @@
+<a href="index.php?uc=accueil">Accueil</a> - 
+<a href="index.php?uc=produit">Produit</a> - 
+<a href="index.php?uc=categorie">Categorie</a> -
+<a href="index.php?uc=contact">Contact</a> -
