036bd99bfeaec6253cc3ea31e7c5eb6d4e459830
ymelki <67371192+ymelki@users.noreply.github.com>
2024-02-13 15:41:24 +0100

    ajout du foreach des produits

--- vue/ProduitVue.php
+++ vue/ProduitVue.php
@@ -1,3 +1,12 @@
 <br >
 - Liste des produits
+
+<?php
+foreach ($produits as $unProduit){ ?>
+        <?= "<hr>"; ?>
+        <?=$unProduit['nom']; ?>
+        <?=$unProduit['prix']; ?> €
+
+<?php 
+}
  
